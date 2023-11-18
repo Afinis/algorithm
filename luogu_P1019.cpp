42ab89aef8fe0462d068266d1f0e137880162dcc
Afinis <2422065529@qq.com>
2023-11-18 10:08:46 +0800

    2023年11月18日 10:08:37

--- luogu_P1019.cpp
+++ luogu_P1019.cpp
@@ -1,60 +1,50 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 int n;
 string s[25], r;
 char be;
 map<string, int> vis;
 int concat(string a, string b){
+    int fin = -1;
 	for(int i = 1; i < a.size(); i++){
-		for(int j = 0; j < b.size(); j++){
-			int ap = i, bp = j;
-			while(ap < a.size() && bp < b.size() && b[bp] == a[ap]){
-				ap++;
-				bp++;
-			}
-			if(ap == a.size() && bp != b.size()){
-				return i;
-			}
-		}
+        int ap = i, bp = 0;
+        while(ap < a.size() && bp < b.size() && b[bp] == a[ap]){
+            ap++;
+            bp++;
+        }
+        if(ap == a.size() && bp != b.size()) fin = i;
 	}
-	return -1;
+	return fin;
 }
 
-
 void dfs(string a, int c){
-	if(c==n){
-		if(r.size() < a.size()){
-			r = a;	
-		}
-		return;
-	}
-	for(int i = 0; i < n; i++){
-		if(vis.count(s[i]) ==0){
-			int p = concat(a, s[i]);
-			if(p != -1){
-				vis[s[i]] = 1;
-				dfs( a.substr(0, p)+s[i] , c+1);
-				vis[s[i]] = 0;
-			}
-		}
-	}
+    if(r.size() < a.size()) r = a;
+    for(int i = 0; i < n; i++){
+        if(vis[s[i]] > 0){
+            int p = concat(a, s[i]);
+            if(p != -1){
+                vis[s[i]]--;
+                dfs(a.substr(0, p)+s[i] , c+1);
+                vis[s[i]]++;
+            }
+        }
+    }
 }
 
 int main(){
 	cin>>n;
 	for(int i = 0; i < n; i++){
 		cin>>s[i];
+        vis[s[i]] = 2;
 	}
 	cin>>be;
 	for(int i = 0; i < n; i++){
 		if(be == s[i][0]){
-			vis[s[i]] = 1;
+			vis[s[i]]--;
 			dfs(s[i], 0);
-			vis[s[i]] = 0;
+			vis[s[i]]++;
 		}
 	}
 	cout<<r.size()<<endl;
-	
 	return 0;
 }
